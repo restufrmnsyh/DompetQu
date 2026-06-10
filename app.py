0aa4f1b0888a5ef2383a8c14b65a1d37045cb285
restufrmnsyh <mrestufirmansyah51@gmail.com>
2026-06-10 08:52:18 +0700

    versi awal DompetQu

---
+++ app.py
@@ -0,0 +1,150 @@
+from flask import Flask, render_template, request, redirect
+
+from database.db import (
+    init_db,
+    tambah_transaksi,
+    ambil_semua_transaksi,
+    hitung_saldo,
+    hapus_transaksi,
+    hitung_ringkasan,
+    ambil_target,
+    simpan_target,
+    simpan_budget,
+    ambil_budget
+)
+
+app = Flask(__name__)
+
+init_db()
+
+
+@app.template_filter("rupiah")
+def rupiah(angka):
+    return "{:,.0f}".format(float(angka)).replace(",", ".")
+
+
+@app.route("/")
+def dashboard():
+
+    transaksi = ambil_semua_transaksi()
+
+    saldo = hitung_saldo()
+
+    pemasukan, pengeluaran = hitung_ringkasan()
+
+    return render_template(
+        "dashboard.html",
+        transaksi=transaksi,
+        saldo=saldo,
+        pemasukan=pemasukan,
+        pengeluaran=pengeluaran
+    )
+
+
+@app.route("/tambah", methods=["GET", "POST"])
+def tambah():
+
+    if request.method == "POST":
+
+        tanggal = request.form["tanggal"]
+        jenis = request.form["jenis"]
+        kategori = request.form["kategori"]
+        nominal = int(request.form["nominal"])
+        catatan = request.form["catatan"]
+
+        tambah_transaksi(
+            tanggal,
+            jenis,
+            kategori,
+            nominal,
+            catatan
+        )
+
+        return redirect("/")
+
+    return render_template("tambah.html")
+
+
+@app.route("/hapus/<int:id>")
+def hapus(id):
+
+    hapus_transaksi(id)
+
+    return redirect("/")
+
+
+@app.route("/target", methods=["GET", "POST"])
+def target():
+
+    if request.method == "POST":
+
+        nama_target = request.form["nama_target"]
+
+        target_nominal = int(
+            request.form["target_nominal"]
+        )
+
+        nominal_terkumpul = int(
+            request.form["nominal_terkumpul"]
+        )
+
+        simpan_target(
+            nama_target,
+            target_nominal,
+            nominal_terkumpul
+        )
+
+        return redirect("/target")
+
+    target_data = ambil_target()
+
+    progress = 0
+
+    if target_data:
+
+        target_nominal = target_data[2]
+        nominal_terkumpul = target_data[3]
+
+        if target_nominal > 0:
+
+            progress = (
+                nominal_terkumpul /
+                target_nominal
+            ) * 100
+
+    return render_template(
+        "target.html",
+        target=target_data,
+        progress=progress
+    )
+
+@app.route("/budget", methods=["GET", "POST"])
+def budget():
+
+    if request.method == "POST":
+
+        bulan = request.form["bulan"]
+
+        jumlah_budget = int(
+            request.form["jumlah_budget"]
+        )
+
+        simpan_budget(
+            bulan,
+            jumlah_budget
+        )
+
+        return redirect("/budget")
+
+    data_budget = ambil_budget()
+
+    pemasukan, pengeluaran = hitung_ringkasan()
+
+    return render_template(
+        "budget.html",
+        budget=data_budget,
+        pengeluaran=pengeluaran
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ database/db.py
@@ -0,0 +1,220 @@
+import sqlite3
+
+def init_db():
+
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS transaksi (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        tanggal TEXT NOT NULL,
+        jenis TEXT NOT NULL,
+        kategori TEXT NOT NULL,
+        nominal INTEGER NOT NULL,
+        catatan TEXT
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS target_tabungan (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nama_target TEXT NOT NULL,
+        target_nominal INTEGER NOT NULL,
+        nominal_terkumpul INTEGER DEFAULT 0
+    )
+    """)
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS budget (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        bulan TEXT NOT NULL,
+        jumlah_budget INTEGER NOT NULL
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+
+def tambah_transaksi(tanggal, jenis, kategori, nominal, catatan):
+
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    INSERT INTO transaksi
+    (tanggal, jenis, kategori, nominal, catatan)
+    VALUES (?, ?, ?, ?, ?)
+    """, (tanggal, jenis, kategori, nominal, catatan))
+
+    conn.commit()
+    conn.close()
+
+
+def ambil_semua_transaksi():
+
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    SELECT *
+    FROM transaksi
+    ORDER BY id DESC
+    """)
+
+    data = cursor.fetchall()
+
+    conn.close()
+
+    return data
+
+
+def hitung_saldo():
+
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    SELECT jenis, nominal
+    FROM transaksi
+    """)
+
+    data = cursor.fetchall()
+
+    conn.close()
+
+    saldo = 0
+
+    for jenis, nominal in data:
+
+        if jenis == "Pemasukan":
+            saldo += nominal
+        else:
+            saldo -= nominal
+
+    return saldo
+
+
+def hapus_transaksi(id):
+
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    DELETE FROM transaksi
+    WHERE id = ?
+    """, (id,))
+
+    conn.commit()
+    conn.close()
+
+
+def hitung_ringkasan():
+
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    SELECT jenis, nominal
+    FROM transaksi
+    """)
+
+    data = cursor.fetchall()
+
+    conn.close()
+
+    pemasukan = 0
+    pengeluaran = 0
+
+    for jenis, nominal in data:
+
+        if jenis == "Pemasukan":
+            pemasukan += nominal
+
+        elif jenis == "Pengeluaran":
+            pengeluaran += nominal
+
+    return pemasukan, pengeluaran
+
+
+def ambil_target():
+
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    SELECT *
+    FROM target_tabungan
+    LIMIT 1
+    """)
+
+    data = cursor.fetchone()
+
+    conn.close()
+
+    return data
+
+
+def simpan_target(
+    nama_target,
+    target_nominal,
+    nominal_terkumpul
+):
+
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    DELETE FROM target_tabungan
+    """)
+
+    cursor.execute("""
+    INSERT INTO target_tabungan
+    (
+        nama_target,
+        target_nominal,
+        nominal_terkumpul
+    )
+    VALUES (?, ?, ?)
+    """,
+    (
+        nama_target,
+        target_nominal,
+        nominal_terkumpul
+    ))
+
+    conn.commit()
+    conn.close()
+
+def simpan_budget(bulan, jumlah_budget):
+
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("DELETE FROM budget")
+
+    cursor.execute("""
+    INSERT INTO budget
+    (bulan, jumlah_budget)
+    VALUES (?, ?)
+    """, (bulan, jumlah_budget))
+
+    conn.commit()
+    conn.close()
+
+def ambil_budget():
+
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    SELECT *
+    FROM budget
+    LIMIT 1
+    """)
+
+    data = cursor.fetchone()
+
+    conn.close()
+
+    return data
